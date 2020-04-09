3b0e8e9a0d8fea7f7f1458e3010b8b1a913db009
Vizzzka <usachevavaika171@gmail.com>
2020-04-09 17:46:48 +0300

    take away print

--- vizzzka.js
+++ vizzzka.js
@@ -1,15 +1,13 @@
 function transformTune(freq, expectedFreq) {
     let t;
-    // let percent = freq / expectedFreq;
     let percent = Math.round(100 * freq / expectedFreq);
-    console.log(percent);
+    // console.log(percent);
     if (percent === 0) t = '-';
     else if (percent === 100) t = 'OK';
     else if (percent <= 97) t = '>>•';
     else if (percent < 100) t = '>•';
     else if (percent < 103) t = '•<';
     else t = '•<<';
-    console.log(t);
     return t;
 }
 
@@ -23,10 +21,3 @@ function tune(arr) {
     return res;
 }
 
-// tune([0, 246.94, 0, 0, 0, 80]) ➞ [" - ", "OK", " - ", " - ", " - ", ">>•"]
-//tune([329.63, 246.94, 195, 146, 111, 82.41]) ➞ ["OK", "OK", ">•", ">•", "•<", "OK" ]
-// tune([329.63, 246.94, 196, 146.83, 110, 82.41]) ➞ ["OK", "OK", "OK", "OK", "OK", "OK"]
-
-console.log(tune([0, 246.94, 0, 0, 0, 80]));
-console.log(tune([329.63, 246.94, 195, 146, 111, 82.41]));
-console.log(tune([329.63, 246.94, 196, 146.83, 110, 82.41]));
